c3b6518ae8db77804d8d6d446e3d15ce18d3fc9e
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-11 23:25:07 +0530

    updating score

--- Pong.py
+++ Pong.py
@@ -6,6 +6,10 @@ t.bgcolor("lightblue")
 t.setup(width=800, height=600)
 t.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -42,6 +46,7 @@ pen.penup()
 pen.hideturtle()
 pen.goto(0, 260)
 pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Movements
 def paddle_a_up():
@@ -93,9 +98,15 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write(f"Player A: {score_a}  Player B: {score_b}", align="center", font=("Courier", 24, "normal"))
 
     # Paddle and Ball Collision
     if (340 < ball.xcor() < 350) and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 40):
